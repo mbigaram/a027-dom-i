e4057bf504653d40cb0128f7fea55bc2723dd99f
mbiga <mbigaram.mb@gmail.com>
2022-10-03 16:38:04 -0300

    Exercicios ok

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 const frutas = ["laranja", "limão", "uva"];
+
+let listaFrutas = document.getElementsByTagName("li")
+
+for (let tamanhoArray in frutas){
+    listaFrutas[tamanhoArray].innerHTML = frutas[tamanhoArray]
+}
+
+let fruitNew = document.getElementById("fruit")
+console.log(fruitNew.value)
+
+const adicionaItem = () => {
+    let fruta4 = document.getElementById("fruta-4")
+    fruta4.innerHTML = fruitNew.value
+}
+
+console.log (fruitNew)
+
+
+
+
+
+
+
+
+
